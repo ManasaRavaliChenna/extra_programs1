8a75310c357c7b44aa081d64a8d31ab7bdc1445a
ManasaRavaliChenna <ManasaRavaliChenna>
2015-12-24 00:19:30 +0530

    linked list programs

--- 2015_30_11/MergeLL.cpp
+++ 2015_30_11/MergeLL.cpp
@@ -1,3 +1,5 @@
+/*2.Merge two linked list which are in sorted order*/
+
 #include<stdio.h>
 #include<conio.h>
 #include<malloc.h>
@@ -6,10 +8,10 @@ struct node
 	int data;
 	struct node *link;
 };
-void insertLast(int,struct node*h);
-void read(struct node*);
-void mergeLLWithoutUsingExtraList(struct node *h1, struct node *h2);
-void display(struct node * ll);
+void insertLast(int,struct node*h);//crating linkedlist
+void read(struct node*);//reading values from input
+void mergeLLWithoutUsingExtraList(struct node *h1, struct node *h2);//merging two linkedlists and storing result in first list
+void display(struct node * ll);//displaying linked list
 int main()
 {
 	struct node *head1 = (struct node*)malloc(sizeof(struct node));
@@ -19,6 +21,7 @@ int main()
 	mergeLLWithoutUsingExtraList(head1, head2);
 	_getch();
 }
+//merging two linkedlists and storing result in first list
 void mergeLLWithoutUsingExtraList(struct node *h1, struct node *h2)
 {
 	if (h1->link == NULL&&h2->link == NULL)
@@ -41,10 +44,12 @@ void mergeLLWithoutUsingExtraList(struct node *h1, struct node *h2)
 		}
 		while (n1->link&&n2)
 		{
+			//manipulating first list when n2 has lesser element than n1
 			if (n1->link->data > n2->data)
 			{
 				if (n1->data != n2->data)
 				{
+					//adding node to the list 
 					struct node *p = (struct node*)malloc(sizeof(struct node));
 					p->data = n2->data;
 					p->link = n1->link;
@@ -54,11 +59,13 @@ void mergeLLWithoutUsingExtraList(struct node *h1, struct node *h2)
 			}
 			n1 = n1->link;
 		}
+		//when is at last node and still there are nodes that are nodes in n1 that arenot visted 
 		if (n2)
 			n1->link = n2;
 	}
 	display(h1->link);
 }
+//merging two linkedlists and storing result in new list
 void mergeLL(struct node *h1, struct node* h2)
 {
 	if (h1->link == NULL&&h2->link == NULL)
@@ -74,6 +81,7 @@ void mergeLL(struct node *h1, struct node* h2)
 		n->link = NULL;
 		n3 = n;
 		struct node *n1 = h1->link, *n2 = h2->link;
+		//storing values in n3 in ascending order checking values in n1 and n2
 		while (n1&&n2)
 		{
 			if (n1->data < n2->data)
@@ -99,19 +107,12 @@ void mergeLL(struct node *h1, struct node* h2)
 				n3 = n3->link;
 			}
 		}
-		while (n1)
-		{
-			n3->data = n1->data;
-			n3->link = (struct node *)malloc(sizeof(struct node));
-			n1 = n1->link;
-		}
-		while (n2)
-		{
-			n3->data = n2->data;
-			n3->link = (struct node *)malloc(sizeof(struct node));
-			n2 = n2->link;
-			n3 = n3->link;
-		}
+		//when there are elements in n1 that have not been traversed
+		if (n1)
+			n3->link = n1;
+		//when there are elements in n2 that have not been traversed
+		if (n2)
+			n3->link = n2;
 		n3->link = NULL;
 		display(n);
 	}
